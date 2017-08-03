ca7ada826ad58c358f798c4e40b2459f7a4e1dd0
Fabien Townsend <fabientownsend@gmail.com>
2017-08-03 19:46:28 +0100

    Display all the users on team member pages

--- lib/hello_web/controllers/teams_controller.ex
+++ lib/hello_web/controllers/teams_controller.ex
@@ -1,11 +1,13 @@
 defmodule HelloWeb.TeamsController do
   use HelloWeb, :controller
   alias Hello.Team
+  alias Hello.User
   alias Hello.Repo
 
   def index(conn, _params) do
     changeset = Team.changeset(%Team{})
     teams = Repo.all(Team)
+
     render(conn, "index.html", teams: teams, changeset: changeset)
   end
 
@@ -15,12 +17,15 @@ defmodule HelloWeb.TeamsController do
 
     changeset = Team.changeset(%Team{})
     teams = Repo.all(Team)
+
     render(conn, "index.html", teams: teams, changeset: changeset)
   end
 
   def members(conn, %{"id" => id}) do
+    application_users = Repo.all(User)
     team = Repo.get(Team, id) |> Repo.preload(:users)
     team_users = Enum.map(team.users, fn x -> x end)
-    render conn, "show.html", team_members: team_users
+
+    render(conn, "show.html", team_members: team_users, application_users: application_users)
   end
 end
